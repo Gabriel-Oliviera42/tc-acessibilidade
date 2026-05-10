34cd28dc1e8f7adb7cfd29878ad7f1f28f3161d2
Gabriel Lourenço de Oliveira <gabriellourenco343@gmail.com>
2026-05-10 20:32:56 -0300

    usa configuracao central no celery

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -1,13 +1,13 @@
-import os
 from celery import Celery
 
-REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
+from core.config import settings
+
 
 celery_app = Celery(
     "motor_acessibilidade",
-    broker=REDIS_URL,
-    backend=REDIS_URL,
-    include=['tasks']  # <--- SÓ ADICIONAR ESTA LINHA AQUI!
+    broker=settings.redis_url,
+    backend=settings.redis_url,
+    include=["tasks"]
 )
 
 celery_app.conf.update(task_track_started=True)
